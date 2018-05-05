ff949eb1cec2f0156ae13b257bd94e2ece9dcebf
Fauricio Rojas Hernandez <fauri-94@hotmail.com>
2018-05-05 17:08:51 -0600

    Avoided calling twice to removeTodo action

--- src/js/components/Todo/Todo.js
+++ src/js/components/Todo/Todo.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
 
-const Todo = ({ todo, toggleFinished, removeTodo }) => (
-  <li>
-    <section>
-      <label className={todo.finished ? 'finished-todo' : ''}
-        onClick={() => toggleFinished(todo.id)}>
-        <input type="checkbox"
-          checked={todo.finished}
-          onClick={() => toggleFinished(todo.id)} />
-        {todo.title}
-      </label>
-      <span className="float-right be-red"
-        onClick={() => removeTodo(todo.id)}>&times;</span>
-    </section>
-  </li>
-);
+const Todo = ({ todo, toggleFinished, removeTodo }) => {
+  const onClick = (e, id) => {
+    e.preventDefault();
+    toggleFinished(id);
+  };
+
+  return (
+    <li>
+      <section>
+        <label className={todo.finished ? 'finished-todo' : ''}
+          onClick={e => onClick(e, todo.id)}>
+          <input type="checkbox"
+            checked={todo.finished}
+            onClick={() => toggleFinished(todo.id)} />
+          {todo.title}
+        </label>
+        <span className="float-right be-red"
+          onClick={() => removeTodo(todo.id)}>&times;</span>
+      </section>
+    </li>
+  );
+};
 
 Todo.propTypes = {
   todo: PropTypes.object.isRequired,
